7b3442ebbc916813ef85ad5ee643fbc4243b15b7
ludemer <55108722+ludemer@users.noreply.github.com>
2023-12-11 12:17:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,22 @@ provider "aws" {
   region     = "us-west-1"
 }
 
+
 terraform {
+  backend "remote" {
+    organization = "hashicorp-v2"
+  }
+
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.27.0"
+    github = {
+      source  = "integrations/github"
+      version = "5.42.0"
     }
   }
+
+  required_version = ">= 0.13.5"
 }
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "test-terrat-credibankInfo"
- # acl    = "private"
-#tags = {
- #   Name        = "My bucket"
-  #  Environment = "Dev"
-  #}
+
+provider "github" {
+  owner = "hashicorp"
 }
