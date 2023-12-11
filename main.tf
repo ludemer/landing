ccd15fa8928ceea94c18da8fa75de871063f04d5
ludemer <55108722+ludemer@users.noreply.github.com>
2023-12-11 11:28:15 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   profile    = "default"
   region     = "us-west-1"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.27.0"
+    }
+  }
+}
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "test-terrat-credibankInfo"
  # acl    = "private"
