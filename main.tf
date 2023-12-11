b3c2830f85bbd691a8f635d11a3254ef4548247f
ludemer <55108722+ludemer@users.noreply.github.com>
2023-12-11 17:19:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,3 @@ provider "aws" {
   profile    = "default"
   region     = "us-west-1"
 }
-
-
-terraform {
-  #backend "remote" {
-   # organization = "hashicorp-v2"
-  #}
-
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "5.42.0"
-    }
-  }
-     
-  #required_version = ">= 0.13.5"
-}
-
-provider "github" {
-  owner = "hashicorp"
-}
